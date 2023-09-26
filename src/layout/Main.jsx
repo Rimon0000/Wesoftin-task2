b4c8d5bd6ea4aa3d22046319e15fbb6ef881cc12
Your Name <Rimon0000>
2023-09-26 11:08:22 +0600

    navbar done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Home/Footer/Footer";
+import Navbar from "../pages/Home/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
